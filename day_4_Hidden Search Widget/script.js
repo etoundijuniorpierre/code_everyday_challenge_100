009152262b6bcf516dcb161ecd90ef61b8215c47
etoundijuniorpierre <etoundijunior@hotmail.com>
2024-06-13 12:48:48 +0100

    add v_custom projet day_4

--- day_4_Hidden Search Widget/script.js
+++ day_4_Hidden Search Widget/script.js
@@ -1,14 +1,18 @@
 var bar = document.querySelector(".search")
 var loup = document.querySelector(".search-icon")
 var input = document.getElementById("input")
+var container = document.querySelector(".container")
 
 loup.addEventListener('click', () => {
     bar.classList.toggle('active');
     loup.classList.toggle("active");
+    container.classList.toggle("active");
 
     if (loup.classList.contains("active")) {
         input.placeholder = "search...";; // Add class to show placeholder
+
     } else {
         input.placeholder = " ";; // Remove class to hide placeholder
+
     }
 });
