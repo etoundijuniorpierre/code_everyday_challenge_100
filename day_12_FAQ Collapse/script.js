5484dc8b02398b9ce23f0d9afd1c77677984fd94
etoundijuniorpierre <etoundijunior@hotmail.com>
2024-06-18 15:41:23 +0100

    ajout projet day_12

---
+++ day_12_FAQ Collapse/script.js
@@ -0,0 +1,23 @@
+
+    var boxes = document.querySelectorAll(".box");
+
+    boxes.forEach(function (box) {
+        // Select the icons within the current box
+        var croix = box.querySelector(".fa-circle-xmark");
+        var check = box.querySelector(".fa-chevron-down");
+
+        // Add click event to the 'check' icon
+        check.addEventListener('click', function (event) {
+            box.querySelector(".boxOne").style.display="none";
+            box.querySelector(".boxTwo").style.display="block";
+            box.querySelector(".box").classList.add("active");
+        });
+
+        // Add click event to the 'croix' icon
+        croix.addEventListener('click', function (event) {
+            box.querySelector(".boxOne").style.display="block";
+            box.querySelector(".boxTwo").style.display="none";
+            box.querySelector(".box").classList.remove("active");
+        });
+    });
+
