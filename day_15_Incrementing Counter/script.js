701aa2548d7a3409c271f1862c2e9f296b8f415b
etoundijuniorpierre <etoundijunior@hotmail.com>
2024-06-22 11:58:38 +0100

    add v_custom projet day_15

--- day_15_Incrementing Counter/script.js
+++ day_15_Incrementing Counter/script.js
@@ -4,30 +4,40 @@ var youtube = document.querySelector(".youtubeBox span")
 
 var facebook = document.querySelector(".facebookBox span")
 
+var img1 = document.getElementById("img1");
+var img2 = document.getElementById("img2");
+var img3 = document.getElementById("img3");
+
 var spTwitter = 1;
 var spFacebook = 1;
 var spYoutube = 1;
+
+function rand() {
+    var rand = Math.floor(Math.random() * 21) + 1;
+
+    return rand
+}
 
 
 window.addEventListener('DOMContentLoaded', function () {
     setInterval(() => {
 
 if (spTwitter <= 12000) {
-            spTwitter = spTwitter + 190
-            twitter.innerHTML = spTwitter-161
-           
-}
+            spTwitter = spTwitter + 140
+            twitter.innerHTML = spTwitter-41
+           img1.src=`/images/image${rand()}.jpg`
+        }
 
 if (spFacebook <= 5000) {
-            spFacebook = spFacebook + 75
-            youtube.innerHTML = spFacebook-26          
-           
-}
+            spFacebook = spFacebook + 95
+            youtube.innerHTML = spFacebook-36    
+           img2.src=`/images/image${rand()}.jpg`
+        }
 
 if (spYoutube <= 7500) {
-            spYoutube = spYoutube + 155  
-            facebook.innerHTML = spYoutube - 96
-           
-}
-    }, 10);
+            spYoutube = spYoutube + 105  
+            facebook.innerHTML = spYoutube-61
+           img3.src=`/images/image${rand()}.jpg`
+        }
+    }, 100);
 });
