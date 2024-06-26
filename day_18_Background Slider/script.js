21d0c948a7fccde7a220d43a56b3e2a096b60efc
etoundijuniorpierre <etoundijunior@hotmail.com>
2024-06-26 16:40:17 +0100

    add v_custom projet day_18

--- day_18_Background Slider/script.js
+++ day_18_Background Slider/script.js
@@ -3,23 +3,22 @@ var right = document.querySelector(".fa-arrow-right");
 var img1 = document.getElementById("img1");
 var img2 = document.getElementById("img2");
 var img = ['image1.png', 'image2.png', 'image3.png', 'image4.jpg', 'image5.jpg', 'image6.jpg', 'image7.jpg', 'image8.jpg', 'image9.jpg', 'image10.jpg', 'image11.jpg', 'image12.jpg', 'image13.jpg'];
-var currentIndex = 1; 
+var currentIndex = 0; 
 
-img1.src = `./images/${img[1]}`;
-img2.src = `./images/${img[1]}`;
+img1.src = `./images/${img[currentIndex]}`;
+img2.src = `./images/${img[currentIndex]}`;
 
-function updateImages(index) {
-    img1.src = `./images/${img[index]}`;
-    img2.src = `./images/${img[index]}`;
+function updateImages() {
+    setInterval(() => {
+        currentIndex++;
+        if (currentIndex >= img.length) {
+            currentIndex = 0;
+        }
+        img1.src = `./images/${img[currentIndex]}`;
+        img2.src = `./images/${img[currentIndex]}`;
+    }, 2000); 
 }
 
-right.addEventListener("click", (e) => {  
-    currentIndex = (currentIndex + 1) % img.length; 
-    updateImages(currentIndex);
+updateImages();
 
-});
 
-left.addEventListener("click", (e) => {
-    currentIndex = (currentIndex - 1 + img.length) % img.length; 
-    updateImages(currentIndex);
-});
