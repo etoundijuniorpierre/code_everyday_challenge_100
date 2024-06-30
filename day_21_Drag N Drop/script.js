330abc849df9c0b25586f60cb1b50378d4c3e8bd
etoundijuniorpierre <etoundijunior@hotmail.com>
2024-06-30 16:42:49 +0100

    modif day_21

--- day_21_Drag N Drop/script.js
+++ day_21_Drag N Drop/script.js
@@ -1,14 +1,15 @@
-var img = document.querySelector(".boxContainer img");
+var img = document.querySelector(".imgbox");
 var containers = document.querySelectorAll(".boxContainer");
 var draggedElement = document.querySelector("img");
 
 document.addEventListener("DOMContentLoaded", function () {
-    img.addEventListener("dragstart", function (event) {
+   
+img.addEventListener("dragstart", function (event) {
         event.dataTransfer.setData("text/plain", event.target.id);
 
     });
-
     containers.forEach(container => {
+         
         container.addEventListener("dragenter", function (event) {
             event.preventDefault();
             if (event.target.classList.contains("boxContainer")) {
@@ -34,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
             if (event.target.classList.contains("boxContainer")) {
                 event.target.style.border = "1px solid #ccc";
-                event.target.appendChild(draggedElement);
+                event.target.appendChild(img);
                 event.target.style.background = "";
             }
         });
